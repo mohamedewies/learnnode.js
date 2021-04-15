007a0bbed0f16d62b24693bd740c648ff40307fc
Mohamed Ewies <mewies@eg.ibm.com>
2021-04-16 01:03:08 +0200

    Add initial code for ejs template engine

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 const rootDir = require('./util/path');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
