4205be2210ce83f9c52168d10a87ec4b08fd4438
Mohamed Ewies <mewies@eg.ibm.com>
2021-04-16 00:35:52 +0200

    Finishing handlebars code

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,15 @@ const { products } = require('../routes/admin');
 
 router.get('/', (req, res, next) => {
   
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
+  res.render('shop', 
+  {
+    prods: products, 
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
