c5d5655de21738a705551e9c318fc61b5d72e394
Mohamed Ewies <mewies@eg.ibm.com>
2021-04-19 00:33:31 +0200

    Storing/Fethcing data to/from file

--- controllers/products.js
+++ controllers/products.js
@@ -1,23 +1,25 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', productCSS: true, formsCSS: true});
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body.title);
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', 
-  {
-    prods: products, 
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll((products) => {
+    res.render('shop', 
+    {
+      prods: products, 
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
