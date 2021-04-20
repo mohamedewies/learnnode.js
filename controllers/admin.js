d68539fb13ebf5def4e88b0d19a8f661fa221e61
Mohamed Ewies <mewies@eg.ibm.com>
2021-04-20 02:05:27 +0200

    Registering the routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,13 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', productCSS: true, formsCSS: true});
+  res.render('admin/add-product', 
+    {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      productCSS: true,
+      formsCSS: true
+    });
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -12,14 +18,11 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop', 
+    res.render('admin/products', 
     {
       prods: products, 
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
 };
