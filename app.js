db781d33c8be060356cb1bdc1bd765ec1bf6e10b
Mohamed Ewies <mewies@eg.ibm.com>
2021-05-12 07:35:35 +0200

    Link products with user in mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,15 +17,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-  next();
+  User.findById('609b5c45bffc09135ef33fea')
+    .then(user => {
+      req.user = user;
+      console.log(`User...${JSON.stringify(user)}`);
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.use('/admin', adminRoutes);
