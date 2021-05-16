bb7537d8bc21f14cd08041d413004d36286abf69
Mohamed Ewies <mewies@eg.ibm.com>
2021-05-16 20:38:03 +0200

    Delete items from carts

--- controllers/shop.js
+++ controllers/shop.js
@@ -50,8 +50,11 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items;
       res.render('shop/cart', 
       {
         path: '/cart',
@@ -81,7 +84,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  req.user.deleteItemFromCart(prodId)
+  req.user.removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
