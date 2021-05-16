b765d52ece575f5a0cea85899bdcdb20221534f4
Mohamed Ewies <mewies@eg.ibm.com>
2021-05-16 20:04:11 +0200

    Add to cart

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
   },
   cart: {
     items: [{
-      producId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+      productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
       quantity: {
         type: Number,
         required: true
@@ -20,6 +20,23 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if(cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({productId: product._id, quantity: newQuantity});
+    }
+    console.log(`updatedCartItems: ${JSON.stringify(updatedCartItems)}`);
+    const updatedCart = { items: updatedCartItems };
+    this.cart = updatedCart;
+    return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
 
